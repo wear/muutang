a334d13258ff57de0837e69aad897b642eb9d08f
wear <wear63659220@gmail.com>
2009-11-09 15:43:38 +0800

    add exception notifiter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password    
   include AuthenticatedSystem     
+  include ExceptionNotifiable
   
   
 end
--- config/environment.rb
+++ config/environment.rb
@@ -39,7 +39,10 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   config.i18n.default_locale = :zh
-end
+  
+end                    
+
+ExceptionNotifier.exception_recipients = %w(skung@sandsroad.com)
 
 ActionMailer::Base.smtp_settings = {
    :tls => true,
