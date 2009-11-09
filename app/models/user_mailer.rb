cb4c0a75a2765efd02ddc28955709853e47df6bb
wear <wear63659220@gmail.com>
2009-11-09 14:21:36 +0800

    reset mail url

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,21 +2,21 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     @subject    += '请激活你的帐户'
-    @body[:url]  = "http://dev.mt/activate/#{user.activation_code}"
+    @body[:url]  = "http://www.muutang.com/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += '你的帐户已激活'
-    @body[:url]  = "http://dev.mt/"
+    @body[:url]  = "http://www.muutang.com/"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.login} <#{user.email}>"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "半亩塘 <support@muutang.com>"
+      @subject     = "[亩塘] "
       @sent_on     = Time.now
       @body[:user] = user   
       @content_type = "text/html"   
