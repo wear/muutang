def9c3ef617111b188b9d429eb484f6f2a6ad271
wear <wear63659220@gmail.com>
2009-11-13 11:57:58 +0800

    fix

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -17,30 +17,4 @@ class LandingController < ApplicationController
   end     
       
   
-  def import
-    Attendee.find(:all).each do |attendee|  
-      password = User.generate_new_password
-      @user = User.new(:email => attendee.email,:login => attendee.name,:password =>password,:password_confirmation => password )
-      begin
-      if @user.save
-        attendee.update_attributes(:initial_pwd => password)
-        @user.profile.update_attributes(:name => attendee.name,:website => attendee.website,
-                                        :company => attendee.company,
-                                        :phone_number => attendee.phone_number,:city => attendee.city,
-                                        :state => attendee.state,:country =>  attendee.country,
-                                        :desc => attendee.description,
-                                        :company_category => attendee.company_category,
-                                        :industry => attendee.industry,
-                                        :company_size => attendee.company_size,:title => attendee.title,
-                                        :work_experience => attendee.work_experience,
-                                        :interesting => attendee.interesting)
-      end
-            rescue 
-            end
-    end
-    respond_to do |wants|
-      wants.html { render :layout => false }
-    end
-  end
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,12 @@ class User < ActiveRecord::Base
   has_many :skills 
   
   validates_presence_of     :login
-  validates_length_of       :login,    :within => 3..40
+  validates_length_of       :login,    :within => 2..40
   validates_uniqueness_of   :login
   validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
 
   validates_presence_of     :email
-  validates_length_of       :email,    :within => 6..100 #r@a.wk
+  validates_length_of       :email,    :within => 3..100 #r@a.wk
   validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
