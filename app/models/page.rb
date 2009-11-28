a6bec90f3fcd0b8e63e0b9786038e12357874744
wear <wear63659220@gmail.com>
2009-11-28 23:46:18 +0800

    fix

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,10 +5,7 @@ class Page < ActiveRecord::Base
   validates_presence_of :name
   
   named_scope :cates,:conditions => ['parent_id = ?','0']   
-  
-  def parent_id=(coming_parent_id)
-    0 unless coming_parent_id
-  end
+
   
 end
                                                                    
